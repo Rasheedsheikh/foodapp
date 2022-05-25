1d54aa38259d638ad169ca8f587b2bf59b17e0dd
Rasheedsheikh <100085425+Rasheedsheikh@users.noreply.github.com>
2022-05-25 12:34:18 +0530

    go

---
+++ componentss/navbar.js
@@ -0,0 +1,9 @@
+const navbar=()=>{
+    return `<div id="navbar">
+    <input id="container" type="text" oninput="search()"/>
+    <h3><a href="index.html">Home</a></h3>
+    <h3><a href="famous.html">Day Special</a></h3>
+    <h3><a href="receipes.html">Receipes</a></h3>
+</div>`
+};
+ export default navbar
